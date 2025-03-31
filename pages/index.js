86c77e50b6981ee87b9bbf460833a712502bdd9c
alberto.alonso <alberto.alonso@leadtech.com>
2025-03-31 12:02:09 +0200

    Update content

--- pages/index.js
+++ pages/index.js
@@ -6,32 +6,39 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>SAG eForm</title>
+        <title>Formulaire</title>
         <meta
           name="description"
-          content="Enty Chile with no stress. Enjoy your holidays."
+          content="Enty uk with no stress. Enjoy your holidays."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script async src="/gtm.js" strategy="beforeInteractive" />
-      <noscript><iframe src="//http://www.googletagmanager.com/ns.html?id=GTM-5Z5G8PHK"
-      height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
       <main>
         <div className="img-container">
-          <Image src={"/flag.png"} width={90} height={55} alt="CL flag" />
+          <Image src={"/flag.png"} width={90} height={55} alt="UK flag" />
         </div>
         <div className="backdrop"></div>
         <div className="content">
-          <h1>SAG Chile Online Form</h1>
-          <p>To complete the Chile Customs Affidavit, you need to fill out the online SAG Chile form. The online application is simple to complete. You need to fill out the online form with basic personal and travel information.  Start Online Application Form and get your SAG by email.</p>
+          <h1>United Kingdom ETA</h1>
+          <p>
+            Les visiteurs qui prévoient de séjourner maximum 6 mois au
+            Royaume-Uni peuvent demander leur United Kingdom ETA pour voyager
+            dans le pays.
+          </p>
+          <p>
+            Vous pouvez facilement obtenir votre United Kingdom ETA, ce qui
+            simplifie la procédure d&rsquo;entrée au Royaume-Uni pour les visiteurs
+            titulaires de l&rsquo;autorisation de voyage électronique.
+          </p>
           <div className="btn-wrapper">
-            <a href="https://www.chile-onlineform.com/application">
-              Start Application
+            <a href="https://www.uk-onlinetraveldoc.com/application">
+              Demande en ligne
             </a>
           </div>
           <p class="disclaimer">
-            Legal Disclaimer: this site is not affiliated with the Government or its sponsors.
+            Legal Mentions légales : le site n&apos;est pas affilié au gouvernement
+            ni à ses partenaires officiels.
           </p>
         </div>
       </main>
