9cd19d3439af629315c7c19029afc51d6e1dcc92
alberto.alonso <alberto.alonso@leadtech.com>
2025-03-31 12:59:10 +0200

    new pages2

---
+++ _next/static/MUpR66aU9a-37prpmrq3F/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-764ad67eb862deb0.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
